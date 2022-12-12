ff158b05ef32bea9fbf0092d811be44d114fb74c
zouchangfu <zcf13543354971@163.com>
2022-12-12 17:04:08 +0800

    增加user注释

--- service/user/api/internal/svc/servicecontext.go
+++ service/user/api/internal/svc/servicecontext.go
@@ -14,6 +14,9 @@ type ServiceContext struct {
 func NewServiceContext(c config.Config) *ServiceContext {
 	// 连接数据
 	conn := sqlx.NewMysql(c.Mysql.DataSource)
+
+	// 创建全局服务上下文
+	// 所有的和数据打交道的可以在这里增加配置
 	return &ServiceContext{
 		Config:    c,
 		UserModel: model.NewUserModel(conn, c.CacheRedis),
--- service/user/api/user.go
+++ service/user/api/user.go
@@ -17,20 +17,23 @@ var configFile = flag.String("f", "etc/user-api.yaml", "the config file")
 func main() {
 	flag.Parse()
 
-	// 加载yml配置文件
+	// 一、加载yml配置文件
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	// 读取配置文件，启动服务
+	// 二、根据配置文件，创建server
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
 
-	// 创建全局服务上下文
-	// 1、连接数据库
-	// 2、
+	// 三、创建全局服务上下文
+	// 	1、连接数据库
+	// 	2、创建操作数据库的Model
 	ctx := svc.NewServiceContext(c)
+
+	// 四、注册处理器
 	handler.RegisterHandlers(server, ctx)
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	// 五、启动服务
 	server.Start()
 }
